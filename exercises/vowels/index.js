8ab4b1ad326b6ae7093086c4fd9157bd82670258
Reed Schimmel <reedschimmel@gmail.com>
2018-06-21 17:25:15 -0500

    solved vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//function vowels(str) {
+//	return str.split('').filter(char => {
+//		char = char.toLowerCase();
+//		const isVowel =
+//			char === 'a' ||
+//			char === 'e' ||
+//			char === 'i' ||
+//			char === 'o' ||
+//			char === 'u';
+//		return isVowel;
+//	}).length;
+//}
+
+function vowels(str) {
+	const vowelString = str.match(/[aeiou]/gi);
+	return vowelString ? vowelString.length : 0
+}
 
 module.exports = vowels;
