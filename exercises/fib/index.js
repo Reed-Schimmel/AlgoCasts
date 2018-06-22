d514d8b39e222191f3b685e48382c115094804dc
Reed Schimmel <reedschimmel@gmail.com>
2018-06-21 23:27:28 -0500

    added memoization to fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,23 +8,30 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-
-	const sequence = [0, 1]
-
-	for (let i = 1; i < n; i++) {
-		sequence.push(sequence[0]+ sequence[1]);
-		sequence.shift()
-	}
-
-	return sequence.pop();
-}
-
 //function fib(n) {
-//	if (n < 2) return n;
 //
-//	return fib(n - 1) + fib(n - 2);
+//	const sequence = [0, 1]
+//
+//	for (let i = 1; i < n; i++) {
+//		sequence.push(sequence[0]+ sequence[1]);
+//		sequence.shift()
+//	}
+//
+//	return sequence.pop();
 //}
+
+const fibs = {};
+
+function fib(n) {
+	if (n < 2) return n;
+
+	if (fibs[n]) return fibs[n];
+
+	fibs[n - 1] = fib(n - 1);
+	fibs[n - 2] = fib(n - 2);
+
+	return fibs[n - 1] + fibs[n - 2];
+}
 
 
 module.exports = fib;
