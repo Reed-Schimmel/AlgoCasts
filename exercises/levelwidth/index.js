bfbf6898c97ec3e79ac29a50697f22919b00c073
Reed Schimmel <reedschimmel@gmail.com>
2018-06-26 00:11:37 -0500

    solved levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,28 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	if (root === null) return;
+
+	const levels = [1];
+	const itemQueue = [root,'stop'];
+
+	let count = 0;
+
+	while (itemQueue.length) {
+		item = itemQueue.shift();
+
+		if (item === 'stop' && count !== 0) {
+			levels.push(count);
+			count = 0;
+		} else if (typeof item === typeof root) {
+			itemQueue.push(...item.children);
+			itemQueue.push('stop');
+			count += item.children.length;
+		}
+	}
+
+	return levels;
+}
 
 module.exports = levelWidth;
